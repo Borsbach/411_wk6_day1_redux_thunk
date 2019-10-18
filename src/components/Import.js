61e8811fdc342e32f13f0e43725fdab7243fa3e6
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-10-17 21:08:51 -0500

    fetch working

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { MoreVert } from '@material-ui/icons'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from '@material-ui/core'
+import Menu from './Menu'
 
 const Import = (props) => {
-    // fill out this component
-
-    return (
-        <p>Import Component</p>
-    )
+  // fill out this component
+  
+  return (
+    // <p>Import Component</p>
+    <Container>
+      <Button 
+      variant='contained' 
+      color='primary' 
+      onClick={props.fetchMakes}
+      >Import</Button>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Make</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {props.makes.map((make, index) => (
+            <TableRow key={make.id}>
+              <TableCell component='th'scope='row'>
+                {make.MakeId}
+              </TableCell>
+              <TableCell>{make['MakeName']}</TableCell>
+              <TableCell>
+                <Menu></Menu></TableCell>
+            </TableRow>
+          ))}
+          </TableBody>
+        </Table>
+    </Container>
+  )
 }
 
 export default Import
